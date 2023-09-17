d1dfeb0a0306e42095a31abf538cfc02a2b81aae
Sonu Kushwaha <imsonukushwaha@gmail.com>
2023-09-17 16:22:37 +0530

    add landscapeMode UI

--- ExpenseTrackerApp/lib/widgets/expenses.dart
+++ ExpenseTrackerApp/lib/widgets/expenses.dart
@@ -63,7 +63,9 @@ class _Expenses extends State<Expenses> {
   }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text(
         "No expenses found, Start adding some!",
@@ -90,14 +92,25 @@ class _Expenses extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpense),
-          Expanded(
-            child: mainContent,
-          )
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpense),
+                Expanded(
+                  child: mainContent,
+                )
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpense),
+                ),
+                Expanded(
+                  child: mainContent,
+                )
+              ],
+            ),
     );
   }
 }
